4d7e7808f90c734721e2dc4037516b93944d0227
Vortigen <vortigen@live.com>
2015-04-21 16:32:32 +0300

    Version0.1

---
+++ Team/Team.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Team
+{
+    class Team
+    {
+
+        #region Private Fields
+
+        private string name;
+        private DateTime yearOfCreation;
+        private string stadium;
+        private string owner;
+        private string headCoach;
+        private string webSite;
+
+        #endregion
+
+        public string Name
+        {
+            get 
+            { 
+                return name; 
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("The name is mandatory");
+                }
+                else
+                {
+                    name = value;
+                }
+            }
+        }
+
+        public DateTime YearOfCreation
+        {
+            get
+            {
+                return yearOfCreation;
+            }
+
+            set
+            {
+                if (value > DateTime.Now)
+                    throw new ArgumentException("Year of creation must be in the past");
+                yearOfCreation = value;
+            }
+        }
+
+        public string Stadium
+        {
+            get
+            {
+                return stadium;
+            }
+            set
+            {
+                Stadium = value;
+            }
+        }
+
+        public string Owner
+        {
+            get
+            {
+                return owner;
+            }
+            set
+            {
+                owner = value;
+            }
+        }
+
+        public string HeadCoach
+        {
+            get
+            {
+                return headCoach;
+            }
+            set
+            {
+                headCoach = value;
+            }
+        }
+
+        public string WebSite
+        {
+            get
+            {
+                return webSite;
+            }
+            set
+            {
+                webSite = value;
+            }
+        }
+
+        private string name;
+        private DateTime yearOfCreation;
+        private string stadium;
+        private string owner;
+        private string headCoach;
+        private string webSite;
+
+
+        public void EnterInformation()
+        {
+            Console.Write("Name: ");
+            Name = Console.ReadLine();
+            
+            Console.Write("Year of creation: ");
+            YearOfCreation = DateTime.Parse(Console.ReadLine());
+            
+            Console.Write("Stadion: ");
+            Stadium = Console.ReadLine();
+            
+            Console.Write("Owner: ");
+            Owner = decimal.Parse(Console.ReadLine());
+            
+            Console.Write("Head Coach: ");
+            HeadCoach = Console.ReadLine();
+
+            Console.Write("Web Site: ");
+            webSite = Console.ReadLine();
+        }
+
+        public string GetFormattedInfo()
+        {
+            return Brand + " (" + Model + ") - " +
+                    Mileage + " (" + Price + ") " +
+                    Color;
+        }
+
+        public override string ToString()
+        {
+            return GetFormattedInfo();
+        }
+    }
+}
---
+++ Team/TeamDemo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamDemo
+{
+    class Program
+    {
+        #region Public Methods
+
+        static void Main(string[] args)
+        {
+            var carsCount = ReadCarsCount();
+
+            var cars = new Car[carsCount];
+            EnterCarsInformation(cars);
+
+            DisplayCarsTable(cars);
+
+            Console.ReadLine();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static int ReadCarsCount()
+        {
+            Console.Write("Въведете брой коли: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static void EnterCarsInformation(IList<Car> cars)
+        {
+            for (int carIndex = 0; carIndex < cars.Count; carIndex++)
+            {
+                Console.WriteLine("Въведете информация за кола " + (carIndex + 1));
+                Car newCar = new Car();
+                newCar.EnterInformation();
+
+                cars[carIndex] = newCar;
+            }
+        }
+
+        private static void DisplayCarsTable(IList<Car> cars)
+        {
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.GetFormattedInfo());
+            }
+        }
+
+        #endregion
+    }
+}
